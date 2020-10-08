abfd7c732ed4748a6364f9e14792fec7b2b066ce
pajid9763 <pajid9763@wrdsb.ca>
2020-10-08 15:25:51 +0000

    finished program that calculates dot product

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * program that calculates dot product
+ * @author Darian
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+
+    double vectors1[] = new double[3];
+    double vectors2[] = new double [3];
+
+    // ask the user for 3 numbers for vector 1
+    System.out.println("Please enter the 3 values for the first vector");
+    for (int i = 0; i < vectors1.length; i++){
+      vectors1[i] = input.nextDouble();
+    }
     
+    // ask the user for 3 numbers for vector 2
+    System.out.println("Please enter the 3 values for the second vector");
+    for (int i = 0; i < vectors2.length; i++){
+      vectors2[i] = input.nextDouble();
+    }
+
+    int total = 0;
+
+    // find the sum
+    for (int i = 0; i < vectors1.length; i++){
+      double sum = vectors1[i] * vectors2[i];
+      total += sum;
+    }
+    System.out.println("The dot product is " + total);
   }
 }
